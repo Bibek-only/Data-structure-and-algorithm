b8d9981af07e2b9ce21e00267c8d69073bd9a36e
Bibek-only <bibekbibek966@gmail.com>
2025-01-25 15:17:23 +0530

    stock span

---
+++ stack/question/questions/_07stockSpan.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+public class _07stocksSpan{
+    public static void findSpan(int stocks[], int span[]){
+       Stack<Integer> st = new Stack<>();
+       span[0] = 1;
+       st.push(0);
+
+       for(int i = 1; i<stocks.length; i++){
+            int currentPrice = stocks[i];
+            while(!st.isEmpty() && currentPrice > stocks[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                span[i] = i+1;
+            }else{
+                int previosHighStockIndex = st.peek();
+                span[i] = i - previosHighStockIndex;
+            }
+            st.push(i);
+       }
+    }
+    public static void main(String args[]){
+        int stocks[] = {100, 80, 60, 70, 60, 85, 100};
+        int span[] = new int[stocks.length];
+        findSpan(stocks,span);
+        for(int i = 0; i<stocks.length; i++){
+            System.out.print(stocks[i]+" ");
+            
+        }
+        System.out.println();
+        for(int i = 0; i<span.length; i++){
+            System.out.print(span[i]+" ");
+
+        }
+        System.out.println();
+
+    }
+}
