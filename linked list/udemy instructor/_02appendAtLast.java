728b842b0845ffb444242e34d070f3343f56335f
Bibek-only <bibekbibek966@gmail.com>
2025-08-21 18:28:18 +0530

    append atd last

---
+++ linked list/udemy instructor/_02appendAtLast.java
@@ -0,0 +1,53 @@
+class LinkedList{
+    private Node head;
+    private Node tail;
+    private int length;
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getHeadData(){
+        return head.data;
+    }
+    public int getTailData(){
+        return tail.data;
+    }
+    //constructor to create the linked list
+    public LinkedList(int data){
+        Node newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+    /*
+     * Here is the append method that append a node at the end of the linked list
+     * 
+     */
+    public void appendAtLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+    public static void main(String args[]){
+        //here create the object of the LinkedList to create the actual linked list
+        LinkedList ll = new LinkedList(1);
+        ll.appendAtLast(2);
+        ll.appendAtLast(3);
+        ll.appendAtLast(4);
+        ll.appendAtLast(5);
+    }
+}
