1132f0ce24e22dc8c32d78687a2f0b0485ae7190
Bibek-only <bibekbibek966@gmail.com>
2025-10-21 14:26:29 +0530

    array have common or not

---
+++ Hash Tables/findArryElementCommonOrNot.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+public class findArryElementCommonOrNot {
+
+    public static boolean isArryHaveCommon(int[] arr1, int[] arr2){ //this method findout is there any common element present or not
+        HashMap<Integer, Boolean> hs = new HashMap<>();
+        
+        for(int i = 0; i<arr1.length; i++){
+            hs.put(arr1[i],true);
+        }
+
+        for(int j = 0; j<arr2.length;j++){
+            if(hs.get(arr2[j]) != null){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,4};
+        int arr2[] = {5,6,7,8,9};
+
+        System.out.println(isArryHaveCommon(arr1, arr2));
+    }
+}
