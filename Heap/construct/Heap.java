072b3048d01f704627b55976376171ca8d9fa5d9
Bibek-only <bibekbibek966@gmail.com>
2025-11-01 01:55:46 +0530

    insert into heap

--- Heap/construct/Heap.java
+++ Heap/construct/Heap.java
@@ -11,7 +11,7 @@ public class Heap {
     // helper functions
 
     // 1. get heap
-    private List<Integer> getHeap() {
+    public ArrayList<Integer> getHeap() {
         return new ArrayList<>(this.heap);
     }
 
@@ -28,6 +28,41 @@ public class Heap {
     // 4. get the parent of a child
     private int getParentIndex(int childIndex) {
         return (int) (childIndex - 1) / 2;
+    }
+
+    // swap two node
+    private void swap(int node1Index, int node2Index) {
+        int temp = heap.get(node1Index);
+        heap.set(node1Index, heap.get(node2Index)); // in the place of the first index put the second index value
+        heap.set(node2Index, temp); // in the place of the second index put the second index value
+    }
+
+    // insert an node in the heap
+
+    public void insert(int value) {
+        heap.add(value); // insert the new value at the last to maintain the complete tree nature of the
+                         // heap
+        int currIndex = heap.size() - 1; // get the index of the new node
+
+        // run the loop until the new node is get in 0th place means it sithe highest
+        // number or then new node placed in the right position where its parent is
+        // greater that it
+        while (currIndex > 0 && heap.get(currIndex) > heap.get(getParentIndex(currIndex))) { // heap.get(currIndex) get
+                                                                                             // the value of the new
+                                                                                             // node, and the
+                                                                                             // heap.get(getParentIndex(currIndex))
+                                                                                             // it get the value of the
+                                                                                             // parent of the current or
+                                                                                             // the new node
+
+            // swap the curent node with its parent if parend is small the current node
+            int parentIndex = getParentIndex(currIndex);
+            swap(currIndex, parentIndex);
+
+            // now update the current to the new node's new place or new updated index
+            currIndex = getParentIndex(currIndex);
+        }
+
     }
 
 }
--- Heap/construct/Main.java
+++ Heap/construct/Main.java
@@ -1,6 +1,20 @@
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Heap myHeap = new Heap();
+
+        myHeap.insert(99);
+        myHeap.insert(72);
+        myHeap.insert(61);
+        myHeap.insert(58);
+        myHeap.insert(55);
+        myHeap.insert(27);
+        myHeap.insert(18);
+
+        ArrayList<Integer> newHeap = myHeap.getHeap();
+        System.out.println(newHeap); 
+        myHeap.insert(100);
+        newHeap = myHeap.getHeap();
+        System.out.println(newHeap);
     }
 }
