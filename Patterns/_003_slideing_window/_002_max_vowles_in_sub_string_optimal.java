900ff803ec96375e4fd203cfa038f100727fea6c
Bibek-only <bibekbibek966@gmail.com>
2026-05-14 20:54:05 +0530

    max vowel in substring

--- Patterns/_003_slideing_window/_002_max_vowles_in_sub_string_optimal.java
+++ Patterns/_003_slideing_window/_002_max_vowles_in_sub_string_optimal.java
@@ -1,4 +1,5 @@
-/*Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+/*
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 
 Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
 
@@ -21,8 +22,8 @@ Output: 2
 Explanation: "lee", "eet" and "ode" contain 2 vowels.
  */
 
-class solution {
-    public static boolean isVowel(char ch) { // helper function to check itis vowel of not
+class Solution {
+    public static boolean isVowel(char ch) {
         if (ch == 'a' ||
                 ch == 'e' ||
                 ch == 'i' ||
@@ -36,31 +37,38 @@ class solution {
 
     public static int maxVowels(String s, int k) {
         int len = s.length();
-        int si = 0;
-        int ei = k - 1;
-        int maxCount = 0;
-        int currentCount = 0;
 
-        while (ei < len) {
-            currentCount = 0;
-            for (int i = si; i <= ei; i++) { // find the current windwos maximum number of vowels
-                if (isVowel(s.charAt(i))) {
-                    currentCount++;
-                }
+        int count = 0;
+        int ans = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
             }
-            if (currentCount > maxCount) {
-                maxCount = currentCount;
-            }
-            si++;
-            ei++;
         }
-        return maxCount;
+
+        ans = count;
+
+        for (int i = k; i < len; i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
+            }
+            if (isVowel(s.charAt(i - k))) {
+                count--;
+            }
+            if (count > ans) {
+                ans = count;
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
-        String str = "aefibcei";
+        String str = "aeiou";
         int k = 3;
         int ans = maxVowels(str, k);
         System.out.println(ans);
+
     }
 }
